23d3cb320b7ee306dd4ef0add1814e529a4111f2
rikeM95 <rike.meyer95@gmx.de>
2024-09-01 21:34:40 +0200

    Delete wp-content directory

--- wp-content/plugins/top-bar/inc/topbar-text-domain.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* Loads plugin's text domain. */
-add_action('plugins_loaded', 'tpbr_load_plugin_textdomain');
-function tpbr_load_plugin_textdomain() {
-  load_plugin_textdomain(TPBR_TXTDM, false, basename(dirname(__FILE__)).'/lang/');
-}
